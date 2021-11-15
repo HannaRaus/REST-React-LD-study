98cce3f624f8d7fadfb7152da2b2d1e231da002f
Hanna Raus <hanna.raus@globallogic.com>
2021-11-15 22:04:58 +0200

    Initial commit

---
+++ src/test/java/com/ld/LD/LdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ld.LD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
