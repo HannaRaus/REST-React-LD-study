16a14a5ca0674d4952580052b678d970fe598b38
Hanna Raus <hanna.raus@globallogic.com>
2022-01-06 22:59:05 +0200

    add spring security

---
+++ src/main/java/com/ld/configuration/SpringSecurityConfiguration.java
@@ -0,0 +1,61 @@
+package com.ld.configuration;
+
+import com.ld.authorization.LdUserDetailsService;
+import com.ld.model.User;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+@AllArgsConstructor
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final LdUserDetailsService userDetailsService;
+
+    @Bean(name = "passwordEncoder")
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public User user() {
+        return new User();
+    }
+
+    @Bean
+    public AuthenticationManager ldAuthenticationManager() throws Exception {
+        return authenticationManager();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/users/registration", "/js/*.js", "/error")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("phone")
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin();
+    }
+}
